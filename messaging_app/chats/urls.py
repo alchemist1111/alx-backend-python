586066e5a0962c529fcf602e12dc635f08c25a3a
alchemist1111 <emoyoeugine@gmail.com>
2025-09-20 22:18:35 +0300

    Updates

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -3,14 +3,14 @@ from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
 # Set up the router and register viewsets
-router = DefaultRouter()
-router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
+routers = DefaultRouter()
+routers.register(r'conversations', ConversationViewSet, basename='conversation')
+routers.register(r'messages', MessageViewSet, basename='message')
 
 
 
 
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(routers.urls))
 ]
