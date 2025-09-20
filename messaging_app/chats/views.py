1cac518a4ec28e323b18534e4c97f572a171ef4a
alchemist1111 <emoyoeugine@gmail.com>
2025-09-20 22:04:23 +0300

    Add conversation viewset

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework import status
+from .models import User, Conversation, Message
+from .serializers import UserSerializer, ConversationSerializer, MessageSerializer
 
-# Create your views here.
+# Conversation Viewset
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    
+    def perform_create(self, serializer):
+        # Create a conversation and add participants
+        conversation = serializer.save()
+        participants = self.request.data.get("participants", [])
+        for user_id in participants:
+            user = User.objects.get(user_id=user_id)
+            conversation.participants.add(user)
+    
+    @action(detail=True, methods=['post'])
+    def send_message(self, request, pk=None):
+        conversation = self.get_object()
+        serializer = MessageSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save(sender=request.user, conversation=conversation)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)        
+            
+                    
