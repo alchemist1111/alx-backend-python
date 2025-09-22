570203af7a150d58a358d0c6459f60dc82746670
alchemist1111 <emoyoeugine@gmail.com>
2025-09-22 16:46:51 +0300

    Add urls for token verification and refresh

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -3,6 +3,11 @@ from django.urls import path, include
 from rest_framework import routers
 from rest_framework_nested import routers as nested_routers
 from .views import ConversationViewSet, MessageViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 # Router setup
 router = routers.DefaultRouter()
@@ -21,4 +26,9 @@ conversations_router.register(r"messages", MessageViewSet, basename="conversatio
 urlpatterns = [
     path('', include(router.urls)),
     path("", include(conversations_router.urls)),
+    
+    # Token
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
