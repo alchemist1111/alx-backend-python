05003d301ec463e0a787eea5e0b806287e4d7d40
alchemist1111 <emoyoeugine@gmail.com>
2025-10-02 14:34:38 +0300

    Add tests message and notification

--- Django-signals_orm-0x04/messaging/tests.py
+++ Django-signals_orm-0x04/messaging/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+# messaging/tests.py
 
-# Create your tests here.
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Message, Notification
+
+class MessageNotificationTest(TestCase):
+
+    def setUp(self):
+        # Create users for testing
+        self.sender = User.objects.create_user(full_name='sender', password='password123')
+        self.receiver = User.objects.create_user(full_name='receiver', password='password123')
+
+    def test_notification_creation_on_message_send(self):
+        # Send a message from sender to receiver
+        message = Message.objects.create(sender=self.sender, receiver=self.receiver, content="Hello, World!")
+
+        # Check if a notification is created for the receiver
+        notification = Notification.objects.get(user=self.receiver)
+        self.assertEqual(notification.message, message)
+        self.assertFalse(notification.seen) 
